5d058376352f144a6c99df078ad73f4d85891c7f
ersen <70ozturkersen@gmail.com>
2022-05-30 21:50:25 +0300

    main templates created

--- main/settings.py
+++ main/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
+    'users',
+    'blog',
+    #3rd party 
 ]
 
 MIDDLEWARE = [
