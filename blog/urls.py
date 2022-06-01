98b986cb97593979af38f6fb70d0a2431034319f
Mikaildokgoz <93521628+Mikaildokgoz@users.noreply.github.com>
2022-06-01 23:19:10 +0300

    Revert "v1"

--- blog/urls.py
+++ blog/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path('list_post', post_list , name='list'),
     path('list_detail/<int:id>', post_detail , name='detail'),
     path('post_update/<int:id>', post_update , name='update'),
+    
+
+
 ]
