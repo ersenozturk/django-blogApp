ec8f060d16ae4b43577e1409af1a94f18c75b24f
ersen <70ozturkersen@gmail.com>
2022-06-11 18:18:01 +0300

    main urls.py revised

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
-"""main URL Configuration
+"""cblog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import post_list as home
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", home, name = "home"),
+    path('', include('blog.urls')),
     path('users/', include('users.urls')),
-    path('blog/', include('blog.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
